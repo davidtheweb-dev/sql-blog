bf76327d6bc55e23eb7db8dc1d2edf99f4956018
davidheweb <davidhermosilladuro@gmail.com>
2022-07-25 19:43:13 +0200

    Deleting blog posts and final improvements

--- routes/blog.js
+++ routes/blog.js
@@ -25,22 +25,6 @@ router.get("/new-post", async function (req, res) {
   res.render("create-post", { authors: authors });
 });
 
-router.post("/posts", async function (req, res) {
-  const data = [
-    req.body.title,
-    req.body.summary,
-    req.body.content,
-    req.body.author,
-  ];
-
-  await db.query(
-    "INSERT INTO posts (title, summary, body, author_id) VALUES (?)",
-    [data]
-  );
-
-  res.redirect("/posts");
-});
-
 router.get("/posts/:id", async function (req, res) {
   const query = `
   SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts 
@@ -83,6 +67,22 @@ router.get("/posts/:id/edit", async function (req, res) {
   res.render("update-post", { post: posts[0] });
 });
 
+router.post("/posts", async function (req, res) {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+
+  await db.query(
+    "INSERT INTO posts (title, summary, body, author_id) VALUES (?)",
+    [data]
+  );
+
+  res.redirect("/posts");
+});
+
 router.post("/posts/:id/edit", async function (req, res) {
   const query = `
     UPDATE posts SET title = ?, summary = ?, body = ?
@@ -95,6 +95,12 @@ router.post("/posts/:id/edit", async function (req, res) {
     req.body.content,
     req.params.id,
   ]);
+
+  res.redirect("/posts");
+});
+
+router.post("/posts/:id/delete", async function (req, res) {
+  await db.query("DELETE FROM posts WHERE id = ?", [req.params.id]);
 
   res.redirect("/posts");
 });
