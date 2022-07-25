0e9a79198c6f87db5956608a8e84016bc7f49ace
davidheweb <davidhermosilladuro@gmail.com>
2022-07-25 13:24:11 +0200

    Inserting and displaying blog posts
    - Inserting posts data with placeholders (Injecting dynamic data)
    - Fetching & displaying a list of blog posts

--- routes/blog.js
+++ routes/blog.js
@@ -8,13 +8,34 @@ router.get("/", function (req, res) {
   res.redirect("/posts");
 });
 
-router.get("/posts", function (req, res) {
-  res.render("posts-list");
+router.get("/posts", async function (req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name FROM posts 
+    INNER JOIN authors ON posts.author_id = authors.id
+  `;
+  const [posts] = await db.query(query);
+  res.render("posts-list", { posts: posts });
 });
 
 router.get("/new-post", async function (req, res) {
   const [authors] = await db.query("SELECT * FROM authors");
   res.render("create-post", { authors: authors });
+});
+
+router.post("/posts", async function (req, res) {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author,
+  ];
+
+  await db.query(
+    "INSERT INTO posts (title, summary, body, author_id) VALUES (?)",
+    [data]
+  );
+
+  res.redirect("/posts");
 });
 
 module.exports = router;
