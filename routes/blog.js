636cbfeb85fd5e1d627cddb9143f356017205de5
davidheweb <davidhermosilladuro@gmail.com>
2022-07-25 18:24:17 +0200

    Fetching and displaying a single blog post

--- routes/blog.js
+++ routes/blog.js
@@ -13,12 +13,15 @@ router.get("/posts", async function (req, res) {
     SELECT posts.*, authors.name AS author_name FROM posts 
     INNER JOIN authors ON posts.author_id = authors.id
   `;
+
   const [posts] = await db.query(query);
+
   res.render("posts-list", { posts: posts });
 });
 
 router.get("/new-post", async function (req, res) {
   const [authors] = await db.query("SELECT * FROM authors");
+
   res.render("create-post", { authors: authors });
 });
 
@@ -36,6 +39,33 @@ router.post("/posts", async function (req, res) {
   );
 
   res.redirect("/posts");
+});
+
+router.get("/posts/:id", async function (req, res) {
+  const query = `
+  SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts 
+  INNER JOIN authors ON posts.author_id = authors.id
+  WHERE posts.id = ?
+  `;
+
+  const [posts] = await db.query(query, [req.params.id]);
+
+  if (!posts || posts.length === 0) {
+    return res.status(404).render("404");
+  }
+
+  const postData = {
+    ...posts[0],
+    date: posts[0].date.toISOString(),
+    humanReadableDate: posts[0].date.toLocaleDateString("es-ES", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }),
+  };
+
+  res.render("post-detail", { post: postData });
 });
 
 module.exports = router;
